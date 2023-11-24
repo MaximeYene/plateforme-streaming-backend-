a044fb63fbde6a295dbc8f073a161a0e23f52115
Maxime Yene <maximeyene@gmail.com>
2023-11-24 04:29:45 +0100

    wip

--- backend/app.js
+++ backend/app.js
@@ -1,47 +1,44 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const multer = require('multer');
-const bodyParser = require('body-parser');
-
+const cors=require('cors');
+const mongoose=require('mongoose');
+const Song = require('./models/song');
 const app = express();
 
-//Connexion a la base de donnees mongoDB
-mongoose.connect('mongodb+srv://maximeyene:Y5991Jmoo@cluster0.gkug5kv.mongodb.net/?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use(bodyParser.json());
+// Configuration de CORS pour permettre l'accès à l'API depuis tous les domaines
+app.use(cors());
+
+// Middleware pour parser le corps des requêtes
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+// Endpoint de l'API pour les chansons
+app.use('/api/songs', Song);
+
+//Connexion à la base de données MongoDB
+mongoose.connect('mongodb://localhost:27017/musicDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.error('Connexion à MongoDB échouée !'));
+
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Les fichiers téléchargés seront enregistrés dans le dossier "uploads"
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
   },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname); // Le nom du fichier téléchargé sera conservé
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
   }
 });
 
 const upload = multer({ storage: storage });
 
-// Route pour l'upload de fichiers audio
-app.post('http://localhost:3000/uploadSong/', upload.single('audioFile'), async (req, res) => {
-  // Extraction des informations de la chanson depuis la requête
+// Endpoint POST pour l'upload de fichiers audio
+app.post('/upload', upload.single('audioFile'), async (req, res) => {
   const { title, artist } = req.body;
-  const audioFilePath = req.file.path; // Récupérer le chemin du fichier audio
+  const audioFilePath = req.file.path;
 
-  // Utilisez le modèle de chanson pour enregistrer la chanson dans la base de données
   const newSong = new Song({
     title: title,
     artist: artist,
@@ -50,10 +47,48 @@ app.post('http://localhost:3000/uploadSong/', upload.single('audioFile'), async 
 
   try {
     const savedSong = await newSong.save();
-    res.status(201).json(savedSong); // Renvoyer la chanson enregistrée
+    res.status(201).json(savedSong);
   } catch (err) {
-    res.status(400).send(err); // En cas d'erreur, renvoyer un code de statut d'erreur
+    res.status(400).send(err);
   }
 });
+
+// Endpoint GET pour récupérer un fichier audio par son ID
+app.get('/:id', async (req, res) => {
+  try {
+    const song = await Song.findById(req.params.id);
+    res.status(200).sendFile(song.audioFilePath);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
+// Votre modèle MongoDB pour l'audio
+const Audio = require('./models/song'); // Assurez-vous d'avoir un modèle défini pour votre audio
+
+// La route pour télécharger l'audio
+app.get('/audio/:audioId', async (req, res) => {
+  const audioId = req.params.audioId;
+
+  try {
+    const audio = await Audio.findById(audioId);
+
+    if (!audio) {
+      // Gérer le cas où l'audio n'est pas trouvé
+      return res.status(404).json({ message: "Audio not found" });
+    }
+
+    // Envoyer le fichier audio en réponse
+    res.set('Content-Type', audio.mimeType);
+    res.set('Content-Disposition', attachment; filename=${audio.fileName});
+    res.send(Audio.audioData);
+
+  } catch (error) {
+    // Gérer toute erreur survenue lors de la recherche de l'audio
+    res.status(500).json({ message: "Error downloading audio" });
+  }
+});
+
+
 
 module.exports = app;
--- backend/models/song.js
+++ backend/models/song.js
@@ -1,9 +1,19 @@
+// models/song.js
 const mongoose = require('mongoose');
 
-const songSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  artist: { type: String, required: true },
-  file_path: {type: String, required:true}
+const songSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  artist: {
+    type: String,
+    required: true
+  },
+  audioFilePath: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Song', songSchema);
